ff7aabdc9a21e58a4e239a38ffafdc27b649c1fc
Duvan Camilo Amaya Lopez <duvan.amayalopez@unicolombo.edu.co>
2024-04-20 13:15:07 -0500

    Guia 3

---
+++ src/guia3/Guia3.java
@@ -0,0 +1,12 @@
+package guia3;
+
+import VentanaGuia3.VentanaPrincipal;
+
+public class Guia3 {
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+    
+}
